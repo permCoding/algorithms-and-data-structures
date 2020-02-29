688870116114a2157cd9eb539943cd6d489b6f19
permCoding <tt@59.ru>
2020-02-29 12:12:14 +0500

    new_modul

--- part-2/01-Class-Recirsia/module.py
+++ part-2/01-Class-Recirsia/module.py
@@ -24,3 +24,27 @@ class Convert():
         else:
             return int(b[-1]) + 2 * (self.bin_to_dec_rec(b[:-1]))
     
+class Multi():
+
+    def __init__(self, par = 'd', num = '0'):
+        if par == 'b':
+            self.num_bin = str(num)
+            self.num_dec = self.bin_to_dec_rec(self.num_bin)
+        elif par == 'd':
+            self.num_dec = int(num)
+            self.num_bin = self.dec_to_bin_rec(self.num_dec)
+        else:
+            self.num_bin = '0'
+            self.num_dec = '0'
+
+    def bin_to_dec_rec(self, b):
+        if len(b) == 0:
+            return 0
+        else:
+            return int(b[-1]) + 2 * (self.bin_to_dec_rec(b[:-1]))
+
+    def dec_to_bin_rec(self, b):
+        '''
+            рекурсивный метод перевода из 10-ой в 2-ую
+        '''
+        return '1101000'
