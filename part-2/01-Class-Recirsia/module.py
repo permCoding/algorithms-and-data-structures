3ad95b87d2f22a30df37bd04cdf057cb5b3e9d2e
permCoding <tt@59.ru>
2020-02-29 11:21:41 +0500

    task

--- part-2/01-Class-Recirsia/module.py
+++ part-2/01-Class-Recirsia/module.py
@@ -11,11 +11,16 @@ class Convert():
             num_dec += int(self.num_bin[pos])*pow(2, len(self.num_bin)-1-pos)
         self.num_dec = num_dec
 
+    # def bin_to_dec_rec(self, b):
+    #     # return 0 if len(b) == 0 else int(b[0])*pow(2, len(b)-1) + self.bin_to_dec_rec(b[1:])
+    #     if len(b) == 0:
+    #         return 0
+    #     else:
+    #         return int(b[0])*pow(2, len(b)-1) + self.bin_to_dec_rec(b[1:])
+        
     def bin_to_dec_rec(self, b):
-        # return 0 if len(b) == 0 else int(b[0])*pow(2, len(b)-1) + self.bin_to_dec_rec(b[1:])
         if len(b) == 0:
             return 0
         else:
-            return int(b[0])*pow(2, len(b)-1) + self.bin_to_dec_rec(b[1:])
-        
-
+            return int(b[-1]) + 2 * (self.bin_to_dec_rec(b[:-1]))
+    
