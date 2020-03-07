5f1e05be0ab1a2a3ab700ca8256843eadb60d52b
permCoding <tt@59.ru>
2020-03-07 12:03:35 +0500

    1

--- part-2/01-Class-Recirsia/_01.py
+++ part-2/01-Class-Recirsia/_01.py
@@ -1,33 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# ((1* 2 + 1)* 2 + 0)* 2 + 1 = 13
-# 11 0 1
-
-def to_dec_rec(b):
-   if b == '':
-      return 0
-   else:
-      return to_dec_rec(b[:-1])*2 + int(b[-1])
-
-def to_dec(b):
-   d = 0
-   for i in range(len(b)):
-      d += int(b[i]) * 2** (len(b)-1-i)  
-   return d
-
-def to_bin(d):
-   b = ''
-   while d > 0:
-      b = str(d%2) + b
-      d //= 2
-   return b
-
-def to_bin_rec():
-   return '110'
+import _module as m
 
 # b = str(input())
 # print(to_dec(b))
 # print(to_dec_rec(b))
 d = 14
-print(to_bin(d))
+print(m.to_bin(d))
