f18940b81b6ffd4b0a57d222d3975ba0396cbca2
permCoding <tt@59.ru>
2020-03-07 12:24:00 +0500

    class

--- part-2/01-Class-Recirsia/_01.py
+++ part-2/01-Class-Recirsia/_01.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import _module as m
+from _module import Convert
 
-# b = str(input())
-# print(to_dec(b))
-# print(to_dec_rec(b))
-d = 14
-print(m.to_bin(d))
+obj1 = Convert()
+
+d = 13
+print(obj1.to_bin(d))
--- part-2/01-Class-Recirsia/_module.py
+++ part-2/01-Class-Recirsia/_module.py
@@ -1,21 +1,23 @@
-def to_dec_rec(b):
-   if b == '':
-      return 0
-   else:
-      return to_dec_rec(b[:-1])*2 + int(b[-1])
+class Convert():
 
-def to_dec(b):
-   d = 0
-   for i in range(len(b)):
-      d += int(b[i]) * 2** (len(b)-1-i)  
-   return d
+    def to_dec_rec(self, b):
+        if b == '':
+            return 0
+        else:
+            return self.to_dec_rec(b[:-1])*2 + int(b[-1])
 
-def to_bin(d):
-   b = ''
-   while d > 0:
-      b = str(d%2) + b
-      d //= 2
-   return b
+    def to_dec(self, b):
+        d = 0
+        for i in range(len(b)):
+            d += int(b[i]) * 2** (len(b)-1-i)  
+        return d
 
-def to_bin_rec():
-   return '110'
+    def to_bin(self, d):
+        b = ''
+        while d > 0:
+            b = str(d%2) + b
+            d //= 2
+        return b
+
+    def to_bin_rec(self):
+        return '110'
