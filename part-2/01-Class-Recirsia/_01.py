e625e92c6f9d0dbf607c9ea1d438e1adb4bbc353
permCoding <tt@59.ru>
2020-03-07 13:02:58 +0500

    class

--- part-2/01-Class-Recirsia/_01.py
+++ part-2/01-Class-Recirsia/_01.py
@@ -4,6 +4,17 @@
 from _module import Convert
 
 obj1 = Convert()
+obj2 = Convert(15)
 
-d = 13
-print(obj1.to_bin(d))
+print(obj2.bin)
+
+
+# obj1.dec = 29
+# obj1.to_bin()
+# print(obj1.bin)
+
+
+# d = 13
+
+# print(obj1.to_bin(d))
+# print(obj2.to_bin_rec(d))
--- part-2/01-Class-Recirsia/_module.py
+++ part-2/01-Class-Recirsia/_module.py
@@ -1,23 +1,33 @@
 class Convert():
 
+    def __init__(self, d = 0):
+        self.dec = d
+        self.bin = self.to_bin()
+
     def to_dec_rec(self, b):
         if b == '':
             return 0
         else:
             return self.to_dec_rec(b[:-1])*2 + int(b[-1])
 
-    def to_dec(self, b):
+    def to_bin_rec(self, d):
+        if d == 0:
+            return ''
+        else:
+            return self.to_bin_rec(d//2) + str(d%2)
+
+    def to_dec(self):
+        b = self.bin
         d = 0
         for i in range(len(b)):
             d += int(b[i]) * 2** (len(b)-1-i)  
         return d
 
-    def to_bin(self, d):
+    def to_bin(self):
+        d = self.dec
         b = ''
         while d > 0:
             b = str(d%2) + b
             d //= 2
         return b
 
-    def to_bin_rec(self):
-        return '110'
